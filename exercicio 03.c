7b79727b2533aea49c1c6d5c89dff1150d904470
LuisLibardi <106477534+LuisLibardi@users.noreply.github.com>
2022-06-24 21:49:55 -0300

    Create exercicio 03.c

---
+++ exercicio 03.c
@@ -0,0 +1,28 @@
+/*Algoritimo
+
+- variaveis
+
+float t1=0, t2=0, volta, tv
+
+- inicio:
+
+para volta = 1 até volta == 5: ---> for (volta = 1; volta <= 5; volta ++)
+leio tempo da "volta" em tv
+
+t1 = t1 + tv
+
+fim: para
+
+para volta = 1 até volta == 5
+leio tempo da volta em tv
+
+t2 = t2 + tv
+
+fima: para
+
+se (t1 < t2):estudande1 ganhou
+
+senao se (t1 == t2): empate
+senao: estudante 2 ganhou
+
+*/
