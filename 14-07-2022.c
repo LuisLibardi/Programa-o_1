22249a843acc516038b462c6e67032223ed7e228
LuisLibardi <106477534+LuisLibardi@users.noreply.github.com>
2022-07-14 19:21:00 -0300

    Create 14-07-2022.c

---
+++ 14-07-2022.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+	int num, flag, i, vet[50], pos;
+	for (i = 0; i < 50; i++)
+		vet[pos] = rand()%100;
+		
+	for (pos = 0; pos < 50; pos++)
+	
+	for(i=1; i<50; i+=2)
+	{
+		printf("%d %d\n", i, i+1);
+	}
+}
+	
